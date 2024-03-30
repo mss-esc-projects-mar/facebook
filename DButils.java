f3888db1b2f80cb3f607f5a4b02351537dce87be
sivakumar <sivakumar99.cl@gmail.com>
2024-03-30 06:35:52 +0000

    First commit

---
+++ DButils.java
@@ -0,0 +1,9 @@
+hello gusy thise one vi editor now using the 
+ 
+
+
+git --verstion
+
+git init 
+
+git init meing check the git crieted the .git files or not
---
+++ git.java
@@ -0,0 +1,10 @@
+first the install the git in linux server use below commed 
+
+
+yum install git -y
+
+offter that check to the install or not in git
+
+git --version
+
+
