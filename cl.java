cafca568dd8544e3ee36608c812d2692242de47d
sivakumar <sivakumar99.cl@gmail.com>
2024-03-30 06:38:17 +0000

    add cl

---
+++ cl.java
@@ -0,0 +1,10 @@
+git install check thise commeds
+
+
+stageging are to move to the working area below commed use
+
+
+git add . or git add * use the two commeds change the working area to the stageing area 
+
+
+first working show the files are cloure red use the git add . coomeed chnage the git stageing are show in the files coloure green in thise files or trcked files.
