dc07714c2c2f804752a2c96eba4eebcacb079782
sivakumar <sivakumar99.cl@gmail.com>
2024-03-31 18:18:16 +0000

    update in dev branch

--- git.java
+++ git.java
@@ -1,8 +1,15 @@
+
+hello i am in master branch
+
+ could you please check i am correct or master branch or not
+
+
+
 first the install the git in linux server use below commed 
 
 
 yum install git -y
-
+o
 offter that check to the install or not in git
 
 git --version
